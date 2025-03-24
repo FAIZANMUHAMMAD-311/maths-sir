f77590c6ca7e98fe534f55e7bb22689a57e7adf7
FAIZANMUHAMMAD-311 <FAIZANWARIS311@gmail.com>
2025-03-24 12:21:20 +0500

    HomeSection.js

--- src/app/components/HomeSection.js
+++ src/app/components/HomeSection.js
@@ -20,7 +20,7 @@ export default function HomeSection() {
           Unlock Your Math Potential
         </h1>
         <p className="mt-8 text-lg sm:text-xl md:text-2xl text-gray-200 max-w-2xl mx-auto" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200">
-          Professional math and physics tutoring tailored to your needs! I offer personalized support for Matric (Math, Physics), Intermediate (Math, Statistics), and Entrance Exam Prep, focusing on conceptual clarity, problem-solving, and exam strategies. Whether you're building basics or tackling advanced topics, I’ll help you succeed with structured lessons and practice materials. Let’s achieve your academic goals together!
+          Professional math tutoring tailored to your needs. Whether you&apos;re struggling with basics or aiming for advanced concepts, I&apos;m here to help you succeed. 
         </p>
         <div className="mt-12 flex flex-col sm:flex-row justify-center gap-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400">
           <a href="#hire" className="bg-white text-blue-600 px-6 sm:px-8 py-3 rounded-full text-lg font-semibold hover:bg-gray-100 transition duration-300">Hire Me</a>
